48bf8ec31f931ce401a5bbdca66bce33d6272550
Thanassis Sergouniotis <t.sergouniotis@inopus.de>
2017-02-13 15:52:44 +0200

    Clean up

--- src/main/java/com/tsergouniotis/house/repositories/PaymentRepository.java
+++ src/main/java/com/tsergouniotis/house/repositories/PaymentRepository.java
@@ -11,7 +11,6 @@ import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
